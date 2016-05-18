bcc30fe4361a416e2c9721a8242ce84544d02974
Rob <rob@myclinicaloutcomes.com>
2016-05-18 10:48:55 +0100

    Attempt to get ASP.NET 4.6.2 running under project.json/xproj - largely failed so parking this branch

---
+++ src/NJsonApiCore.Web.MVC5.HelloWorld/Controllers/ArticlesController.cs
@@ -0,0 +1,51 @@
+﻿using NJsonApi.Infrastructure;
+using NJsonApi.Web.MVC5.HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace NJsonApi.Web.MVC5.HelloWorld.Controllers
+{
+    [Route("articles")]
+    public class ArticlesController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Article> Get()
+        {
+            return StaticPersistentStore.Articles;
+        }
+
+        [Route(("{id}"))]
+        [HttpGet]
+        public Article Get(int id)
+        {
+            return StaticPersistentStore.Articles.Single(w => w.Id == id);
+        }
+
+        [HttpPost]
+        public Article Post([FromBody]Delta<Article> article)
+        {
+            var newArticle = article.ToObject();
+            newArticle.Id = StaticPersistentStore.GetNextId();
+            StaticPersistentStore.Articles.Add(newArticle);
+            return newArticle;
+        }
+
+        [Route("{id}")]
+        [HttpPatch()]
+        public Article Patch([FromBody]Delta<Article> update, int id)
+        {
+            var article = StaticPersistentStore.Articles.Single(w => w.Id == id);
+            update.ApplySimpleProperties(article);
+            return article;
+        }
+
+        [Route("{id}")]
+        [HttpDelete()]
+        public void Delete(int id)
+        {
+            StaticPersistentStore.Articles.RemoveAll(x => x.Id == id);
+        }
+    }
+}
---
+++ src/NJsonApiCore.Web.MVC5.HelloWorld/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using NJsonApi.Web.MVC5.HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace NJsonApi.Web.MVC5.HelloWorld.Controllers
+{
+    [Route("people")]
+    public class PeopleController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Comment> Get()
+        {
+            return StaticPersistentStore.Comments;
+        }
+
+        [Route("{id}")]
+        [HttpGet()]
+        public Person Get(int id)
+        {
+            return StaticPersistentStore.People.Single(w => w.Id == id);
+        }
+    }
+}
